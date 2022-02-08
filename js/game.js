4b622d86f3dfb1561f6007021c12f738905a134b
adhry <adhrydev@gmail.com>
2022-02-07 23:56:01 -0600

    Adding logic to the first mini

--- js/game.js
+++ js/game.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
-    $("div.card").click(function(){
-        alert("You have choose");
+    $("div.rivalCard").click(function(){
+        alert("Your oppenent choise");
     });
+    $("div#rock").click(function(){
+        $("div#paper").hide()
+        $("div#scissors").hide()
+        let n = Math.floor((Math.random() * 3) + 1);
+        if (n === 1) {
+            $("div#rival img").attr("src","img/rock.png")
+        } else if (n === 3) {
+            $("div#rival img").attr("src","img/paper.png")
+        } else {
+            $("div#rival img").attr("src","img/scissors.png")
+        }
+    });
+    $("div#paper").click(function(){
+        alert("Paper!!");
+    });
+    $("div#scissors").click(function(){
+        alert("Scissors!!");
+    })
 });
